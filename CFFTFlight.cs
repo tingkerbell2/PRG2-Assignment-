23c671296a745eb958f9104291bf96dc1798415c
j <126433449+jq058@users.noreply.github.com>
2025-01-15 22:06:30 +0800

    Add files via upload

---
+++ CFFTFlight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver1
+{
+    public class CFFTFlight:Flight
+    {
+        public double requestFee {  get; set; }
+        public CFFTFlight() { }
+        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+            : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            requestFee = 150; // Fee for CFFT special request
+        }
+        public override double CalculateFees() 
+        {
+            double totalFee = 300; // base fee for boarding gate
+            if(origin == "Singapore (SIN)")
+            {
+                totalFee += 800; //Departing flight fee
+            }
+            if (destination == "Singapore (SIN)")
+            {
+                totalFee += 500; //arriving flight fee
+            }
+            totalFee += requestFee; 
+            return totalFee;
+        }
+        public override string ToString()
+        {
+            return $"\t{requestFee}";
+        }
+    }
+}
