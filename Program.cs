71c087cd3fb4e28ea136c3a3874c516a8b45a6e5
jq058 <jqlau9096@gmail.com>
2025-01-15 22:43:43 +0800

    create display menu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-
+void displayMenu()
+{
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Welcome to Changi Airport Terminal 5");
+    Console.WriteLine("=============================================");
+    Console.WriteLine("1. List All Flights");
+    Console.WriteLine("2. List Boarding Gates");
+    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+    Console.WriteLine("4. Create Flight");
+    Console.WriteLine("5. Display Airline Flights");
+    Console.WriteLine("6. Modify Flight Details");
+    Console.WriteLine("7. Display Flight Schedule");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine();
+    Console.WriteLine("Please select your option:");
+}
