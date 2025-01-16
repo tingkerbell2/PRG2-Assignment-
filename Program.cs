f2ec09904b6817ded97efe042f209bbb36a05cdb
jq058 <jqlau9096@gmail.com>
2025-01-16 23:49:09 +0800

    created loadAirlines() and loadBoardingGates()

--- Program.cs
+++ Program.cs
@@ -14,3 +14,58 @@ void displayMenu()
     Console.WriteLine();
     Console.WriteLine("Please select your option:");
 }
+
+void loadAirlines()
+{
+    using (StreamReader sr = new StreamReader("airlines.csv"))
+    {
+        string header = sr.ReadLine();
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] data = line.Split(",");
+            string airlineCode = data[0];
+            string airlineName = data[1];
+
+            Airline airline = new Airline(airlineCode, airlineName);
+            airlinesDict[airlineCode] = airline;
+        }
+    }
+}
+void loadBoardingGates()
+{
+    using (StreamReader sr = new StreamReader("boardinggates.csv"))
+    {
+        string header = sr.ReadLine();
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] data = line.Split(",");
+            string boardingGate = data[0];
+            bool supportsDDJB = bool.Parse(data[1]);
+            bool supportsCFFT = bool.Parse(data[2]);
+            bool supportsLWTT = bool.Parse(data[3]);
+
+            BoardingGate gate = new BoardingGate(boardingGate, supportsDDJB, supportsCFFT, supportsLWTT);
+            boardinggatesDict[boardingGate] = gate;
+        }
+    }
+}
+void loadFlights()
+{
+    using (StreamReader sr = new StreamReader("flights.csv"))
+    {
+        string header = sr.ReadLine(); //skip header row
+        string line;
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] data = line.Split(',');
+            string flightNumber = data[0];
+            string origin = data[1];
+            string destination = data[2];
+            DateTime expectedTime = DateTime.Parse(data[3]);
+            string specialRequestCode = data[4];
+
+            //Extract airline code from the first 2 characters of the flight number
+            string airlineCode = flightNumber.Substring(0, 2);
