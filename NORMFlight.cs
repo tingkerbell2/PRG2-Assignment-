340432b42ee02c89ce735713d98956b4f4a03744
Ting Li Ee <S10267852@connect.np.edu.sg>
2025-01-17 12:29:39 +0800

    Update NORMFlight.cs

--- NORMFlight.cs
+++ NORMFlight.cs
@@ -1,1 +1,35 @@
-//NORMFlight class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267852_PRG2_Assignment
+{
+    public class NORMFlight : Flight
+    {
+        //Constructor
+        public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        : base(flightNumber, origin, destination, expectedTime, status)
+        {
+        }
+
+        //CalculateFees method
+        public override double CalculateFees()
+        {
+            //baseFee depends if its arrival or departure
+            double baseFee = (destination == "SIN") ? 500 : 800;
+            //constant boardingGateFee
+            const double boardingGateFee = 300;
+            return  baseFee + boardingGateFee;
+        }
+
+        //ToString method
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
+
