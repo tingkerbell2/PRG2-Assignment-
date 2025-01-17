3d7a9a4d069a832ba717e702d153285dcc1da468
Ting Li Ee <S10267852@connect.np.edu.sg>
2025-01-17 12:26:40 +0800

    Update Airline.cs

--- Airline.cs
+++ Airline.cs
@@ -1,1 +1,79 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267852_PRG2_Assignment
+{
+    class Airline
+    {
+        //Properties
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> flights;
+
+        //Default Constructor
+        public Airline() { }
+
+        //Parameterized Constructor
+        public Airline(string Name, string Code)
+        {
+            this.Name = Name;
+            this.Code = Code;
+            flights = new Dictionary<string, Flight>();
+        }
+
+        //Methods 
+        //Add Flight to airline
+        public bool AddFlight(Flight flight)
+        {
+            if (flights.ContainsKey(flight.flightNumber))
+            {
+                Console.WriteLine($"Flight {flight.flightNumber} already exists in the airline");
+                return false;
+            }
+            flights[flight.flightNumber] = flight;
+            Console.WriteLine($"Flight {flight.flightNumber} successfully added to the airline");
+            return true;
+
+        }
+
+        //Calculate Fees
+        public double CalculateFees()
+        {
+            double totalFees = 0;
+            foreach (Flight flight in flights.Values)
+            {
+                totalFees += flight.CalculateFees();
+            }
+            return totalFees;
+        }
+
+        //Remove Flight from airline
+        public bool RemoveFlight(Flight flight)
+        {
+            if (!flights.ContainsKey(flight.flightNumber))
+            {
+                Console.WriteLine($"Error occurred. Flight {flight.flightNumber} does not exist in the airline");
+                return false;
+            }
+
+            flights.Remove(flight.flightNumber);
+            return true;
+        }
+
+        //ToString()
+        public override string ToString()
+        {
+            string str = $"Airline: {Name} ({Code})\n";
+            foreach (Flight flight in flights.Values)
+            {
+                str += flight.ToString() + "\n";
+            }
+            return str;
+        }
+
+    }
+}
