d83f190b45bacc40d312c73756950af3bd3a63de
Ting Li Ee <S10267852@connect.np.edu.sg>
2025-01-18 21:13:16 +0800

    Update DDJBFlight.cs

--- DDJBFlight.cs
+++ DDJBFlight.cs
@@ -1,12 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ver1
+namespace S10267852_PRG2_Assignment
 {
-    internal class DDJBFlight
+    class DDJBFlight: Flight
     {
+        public double requestFee { get; set; }
+        public DDJBFlight() { }
+        public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+            : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            requestFee = 200; // Fee for DDJB special request
+        }
+        public override double CalculateFees()
+        {
+            // base fee for boarding gate
+            double totalFee = 300; 
+            if (origin == "Singapore (SIN)")
+            {
+                totalFee += 800; 
+            }
+            if (destination == "Singapore (SIN)")
+            {
+                totalFee += 500; 
+            }
+            totalFee += requestFee;
+            return totalFee;
+        }
+        public override string ToString()
+        {
+            return $"\t{requestFee}";
+        }
     }
 }
