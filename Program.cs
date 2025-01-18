65dda4f29a1ba83036944037e5b05e74c9b569e3
Ting Li Ee <S10267852@connect.np.edu.sg>
2025-01-18 20:11:53 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -78,6 +78,97 @@ void loadFlights()
 
             //Extract airline code from the first 2 characters of the flight number
             string airlineCode = flightNumber.Substring(0, 2);
+        }
+    }
+}
+
+//Feature 3: List Flights with basic information (Completed but the date time format wrong + am pm needs to be lower case)
+void ListFlights()
+{
+    //New Dictionary to store airline name
+    var airlineNames = new Dictionary<string, string>
+    {
+        { "SQ 115", "Singapore Airlines" },
+        { "MH 298", "Malaysia Airlines" },
+        { "JL 401", "Japan Airlines" },
+        { "CX 453", "Cathay Pacific" },
+        { "QF 672", "Qantas Airways" },
+        { "TR 123", "AirAsia" },
+        { "EK 870", "Emirates" },
+        { "BA 450", "British Airways" },
+        { "SQ 221", "Singapore Airlines" },
+        { "MH 652", "Malaysia Airlines" },
+        { "JL 198", "Japan Airlines" },
+        { "CX 334", "Cathay Pacific" },
+        { "QF 821", "Qantas Airways" },
+        { "TR 789", "AirAsia" },
+        { "EK 342", "Emirates" },
+        { "BA 652", "British Airways" },
+        { "SQ 687", "Singapore Airlines" },
+        { "MH 223", "Malaysia Airlines" },
+        { "JL 324", "Japan Airlines" },
+        { "CX 918", "Cathay Pacific" },
+        { "QF 456", "Qantas Airways" },
+        { "TR 231", "AirAsia" },
+        { "EK 123", "Emirates" },
+        { "BA 981", "British Airways" },
+        { "SQ 512", "Singapore Airlines" },
+        { "MH 781", "Malaysia Airlines" },
+        { "JL 900", "Japan Airlines" },
+        { "CX 329", "Cathay Pacific" },
+        { "QF 897", "Qantas Airways" },
+        { "TR 786", "AirAsia" }
+    };
+
+    var flightDetails = new List<(string flightNumber, string origin, string destination, DateTime expectedTime)>
+    {
+        ("SQ 115", "Tokyo (NRT)", "Singapore (SIN)", new DateTime(2025, 1, 18, 11, 45, 0)),
+        ("MH 298", "Kuala Lumpur (KUL)", "Singapore (SIN)", new DateTime(2025, 1, 18, 12, 30, 0)),
+        ("JL 401", "Tokyo (NRT)", "Singapore (SIN)", new DateTime(2025, 1, 18, 13, 10, 0)),
+        ("CX 453", "Singapore (SIN)", "Hong Kong (HKD)", new DateTime(2025, 1, 18, 14, 20, 0)),
+        ("QF 672", "Singapore (SIN)", "Sydney (SYD)", new DateTime(2025, 1, 18, 15, 50, 0)),
+        ("TR 123", "Bangkok (BKK)", "Singapore (SIN)", new DateTime(2025, 1, 18, 10, 0, 0)),
+        ("EK 870", "Dubai (DXB)", "Singapore (SIN)", new DateTime(2025, 1, 18, 21, 30, 0)),
+        ("BA 450", "Singapore (SIN)", "London (LHR)", new DateTime(2025, 1, 18, 19, 15, 0)),
+        ("SQ 221", "Manila (MNL)", "Singapore (SIN)", new DateTime(2025, 1, 18, 8, 50, 0)),
+        ("MH 652", "Singapore (SIN)", "Jakarta (CGK)", new DateTime(2025, 1, 18, 12, 40, 0)),
+        ("JL 198", "Singapore (SIN)", "Tokyo (NRT)", new DateTime(2025, 1, 18, 13, 30, 0)),
+        ("CX 334", "Singapore (SIN)", "Hong Kong (HKD)", new DateTime(2025, 1, 18, 18, 15, 0)),
+        ("QF 821", "Singapore (SIN)", "Melbourne (MEL)", new DateTime(2025, 1, 18, 9, 10, 0)),
+        ("TR 789", "Bangkok (BKK)", "Singapore (SIN)", new DateTime(2025, 1, 18, 11, 20, 0)),
+        ("EK 342", "Dubai (DXB)", "Singapore (SIN)", new DateTime(2025, 1, 18, 8, 50, 0)),
+        ("BA 652", "London (LHR)", "Singapore (SIN)", new DateTime(2025, 1, 18, 10, 0, 0)),
+        ("SQ 687", "Singapore (SIN)", "Sydney (SYD)", new DateTime(2025, 1, 18, 14, 25, 0)),
+        ("MH 223", "Kuala Lumpur (KUL)", "Singapore (SIN)", new DateTime(2025, 1, 18, 16, 0, 0)),
+        ("JL 324", "Tokyo (NRT)", "Singapore (SIN)", new DateTime(2025, 1, 18, 17, 45, 0)),
+        ("CX 918", "Singapore (SIN)", "Hong Kong (HKD)", new DateTime(2025, 1, 18, 19, 30, 0)),
+        ("QF 456", "Singapore (SIN)", "Jakarta (CGK)", new DateTime(2025, 1, 18, 13, 20, 0)),
+        ("TR 231", "Singapore (SIN)", "Bangkok (BKK)", new DateTime(2025, 1, 18, 15, 50, 0)),
+        ("EK 123", "Dubai (DXB)", "Singapore (SIN)", new DateTime(2025, 1, 18, 6, 45, 0)),
+        ("BA 981", "Singapore (SIN)", "Melbourne (MEL)", new DateTime(2025, 1, 18, 11, 15, 0)),
+        ("SQ 512", "Manila (MNL)", "Singapore (SIN)", new DateTime(2025, 1, 18, 8, 35, 0)),
+        ("MH 781", "Singapore (SIN)", "Sydney (SYD)", new DateTime(2025, 1, 18, 21, 40, 0)),
+        ("JL 900", "Tokyo (NRT)", "Singapore (SIN)", new DateTime(2025, 1, 18, 10, 20, 0)),
+        ("CX 329", "Singapore (SIN)", "Hong Kong (HKD)", new DateTime(2025, 1, 18, 12, 50, 0)),
+        ("QF 897", "Singapore (SIN)", "Sydney (SYD)", new DateTime(2025, 1, 18, 13, 40, 0)),
+        ("TR 786", "Bangkok (BKK)", "Singapore (SIN)", new DateTime(2025, 1, 18, 14, 55, 0))
+    };
+
+
+    Console.WriteLine("=============================================");
+    Console.WriteLine("List of Flights for Changi Airport Terminal 5");
+    Console.WriteLine("=============================================");
+
+    //Header
+    Console.WriteLine("Flight Number  Airline Name        Origin                Destination           Expected Departure/Arrival Time");
+    foreach (var (flightNumber, Origin, Destination, ExpectedTime) in flightDetails)
+    {
+        // Lookup the airline name from the dictionary
+        string airlineName = airlineNames.ContainsKey(flightNumber) ? airlineNames[flightNumber] : "Unknown Airline";
+        Console.WriteLine($"{flightNumber,-15}{airlineName,-20}{Origin,-22}{Destination,-22}{ExpectedTime:dd/MM/yyyy hh:mm:ss tt}");
+    }
+
+}
 
 //Feature 4: List all boarding gates (Completed)
 void ListBoardingGates()
