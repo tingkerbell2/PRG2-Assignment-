8eaab09f4dc84e4fdeca370c25cc9158732d4118
j <126433449+jq058@users.noreply.github.com>
2025-01-26 21:20:07 +0800

    add block comment

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -1,3 +1,8 @@
+	//==========================================================
+	// Student Number	: S10267822
+	// Student Name	: Lau Jia Qi
+	// Partner Name	: Ee Ting Li
+//==========================================================
 namespace S10267852_PRG2_Assignment
 {
     class BoardingGate
