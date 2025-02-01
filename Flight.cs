a958bf5c802962bad519ce52168fdf7ee552d353
Ting Li Ee <S10267852@connect.np.edu.sg>
2025-02-01 16:55:11 +0800

    Update Flight.cs

--- Flight.cs
+++ Flight.cs
@@ -13,29 +13,45 @@ namespace PRG2_Assignment
 {
     public abstract class Flight:IComparable<Flight>
     {
-        public string flightNumber {  get; set; }
-        public string origin {  get; set; }
-        public string destination { get; set; }
-        public DateTime expectedTime { get; set; }
-        public string status { get; set; }
-        public Flight() { }
-        public Flight(string FlightNumber, string Origin, string Destination, DateTime ExpectedTime, string Status)
-        {
-            flightNumber = FlightNumber;
-            origin = Origin;
-            destination = Destination;
-            expectedTime = ExpectedTime;
-            status = "Scheduled";
-        }
-        public abstract double CalculateFees();
-        public override string ToString()
-        {
-            return $"{flightNumber}\t{origin}\t{destination}\t{expectedTime:dd/MM/yyyy hh:mm tt}";
-        }
-        public int CompareTo(Flight other)
-        {
-            return expectedTime.CompareTo(other.expectedTime);
-        }
+            public string flightNumber { get; set; }
+    public string origin { get; set; }
+    public string destination { get; set; }
+    public DateTime expectedTime { get; set; }
+    public string status { get; set; }
+
+    // Added for advanced feature
+    public BoardingGate boardingGate { get; set; }
+
+    public string specialRequestCode { get; set; } 
+
+    public Airline airline { get; set; }
+    public Flight() { }
+
+    public Flight(string FlightNumber, string Origin, string Destination, DateTime ExpectedTime, string Status, BoardingGate boardingGate, string specialRequestCode = null)
+    {
+        flightNumber = FlightNumber;
+        origin = Origin;
+        destination = Destination;
+        expectedTime = ExpectedTime;
+        status = Status;
+        this.boardingGate = boardingGate; 
+        this.specialRequestCode = specialRequestCode;
+        this.airline = airline;
+    }
+
+    public virtual double CalculateFees()
+    {
+        return 0;
+    }
+
+    public override string ToString()
+    {
+        return $"{flightNumber}\t{origin}\t{destination}\t{expectedTime:dd/MM/yyyy hh:mm tt}\t{specialRequestCode}";
+    }
+
+    public int CompareTo(Flight other)
+    {
+        return expectedTime.CompareTo(other.expectedTime);
     }
 }
 
