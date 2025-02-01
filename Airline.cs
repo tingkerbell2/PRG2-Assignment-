79ef5c26b7f67eacb37b64fdc3bcbb7dacb0d55f
Ting Li Ee <S10267852@connect.np.edu.sg>
2025-02-01 16:54:16 +0800

    Update Airline.cs

--- Airline.cs
+++ Airline.cs
@@ -14,71 +14,86 @@ namespace PRG2_Assignment
 {
     class Airline
     {
-        //Properties
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public Dictionary<string, Flight> flights;
+            //Properties
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public Dictionary<string, Flight> flights;
 
-        //Default Constructor
-        public Airline() { }
+    //Default Constructor
+    public Airline() 
+    {
+        flights = new Dictionary<string, Flight>();
+    }
 
-        //Parameterized Constructor
-        public Airline(string Name, string Code)
+    //Parameterized Constructor
+    public Airline(string Name, string Code)
+    {
+        this.Name = Name;
+        this.Code = Code;
+        flights = new Dictionary<string, Flight>();
+    }
+
+    //Methods 
+    //Add Flight to airline
+    public bool AddFlight(Flight flight)
+    {
+        if (flights.ContainsKey(flight.flightNumber))
         {
-            this.Name = Name;
-            this.Code = Code;
-            flights = new Dictionary<string, Flight>();
+            Console.WriteLine($"Flight {flight.flightNumber} already exists in the airline");
+            return false;
         }
 
-        //Methods 
-        //Add Flight to airline
-        public bool AddFlight(Flight flight)
+        // Ensure the flight has an airline assigned before adding it to the collection
+        if (flight.airline == null)
         {
-            if (flights.ContainsKey(flight.flightNumber))
-            {
-                Console.WriteLine($"Flight {flight.flightNumber} already exists in the airline");
-                return false;
-            }
-            flights[flight.flightNumber] = flight;
-            Console.WriteLine($"Flight {flight.flightNumber} successfully added to the airline");
-            return true;
-
+            flight.airline = this;  // Set the current airline as the flight's airline
         }
 
-        //Calculate Fees
-        public double CalculateFees()
+        flights[flight.flightNumber] = flight;
+        Console.WriteLine($"Flight {flight.flightNumber} successfully added to the airline");
+        return true;
+    }
+
+
+    //Calculate Fees
+    public double CalculateFees()
+    {
+        double totalFees = 0;
+        foreach (Flight flight in flights.Values)
         {
-            double totalFees = 0;
-            foreach (Flight flight in flights.Values)
-            {
-                totalFees += flight.CalculateFees();
-            }
-            return totalFees;
+            totalFees += flight.CalculateFees();
+        }
+        return totalFees;
+    }
+
+    //Remove Flight from airline
+    public bool RemoveFlight(Flight flight)
+    {
+        if (!flights.ContainsKey(flight.flightNumber))
+        {
+            Console.WriteLine($"Error occurred. Flight {flight.flightNumber} does not exist in the airline");
+            return false;
         }
 
-        //Remove Flight from airline
-        public bool RemoveFlight(Flight flight)
-        {
-            if (!flights.ContainsKey(flight.flightNumber))
-            {
-                Console.WriteLine($"Error occurred. Flight {flight.flightNumber} does not exist in the airline");
-                return false;
-            }
+        flights.Remove(flight.flightNumber);
+        return true;
+    }
 
-            flights.Remove(flight.flightNumber);
-            return true;
-        }
-
-        //ToString()
-        public override string ToString()
+    //ToString()
+    public override string ToString()
+    {
+        string str = $"Airline: {Name} ({Code})\n";
+        if (flights.Count > 0)
         {
-            string str = $"Airline: {Name} ({Code})\n";
             foreach (Flight flight in flights.Values)
             {
                 str += flight.ToString() + "\n";
             }
-            return str;
         }
-
+        else
+        {
+            str += "No flights available.\n";
+        }
+        return str;
     }
 }
